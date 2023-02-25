3de2f8ad43e224f7c5b99c942f08f860fc66fc1a
Balcihatice <aciceaybike@gmail.com>
2023-02-26 00:01:41 +0300

    Initial commit

---
+++ src/test/java/tests/cross/CrossTest02.java
@@ -0,0 +1,13 @@
+package tests.cross;
+
+import org.testng.annotations.Test;
+import utilities.TestBaseCross;
+
+public class CrossTest02 extends TestBaseCross {
+
+    @Test
+    public void test02() {
+        driver.get("https://www.techproeducation.com");
+
+    }
+}
